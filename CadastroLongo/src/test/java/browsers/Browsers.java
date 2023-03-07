eec98a0485a03c74229bd7ed6cb66d9feb195d27
Igor Roberth <igorroberthm@gmail.com>
2023-03-07 16:02:05 -0300

    Desenvolvimento concluído

--- CadastroLongo/src/test/java/browsers/Browsers.java
+++ CadastroLongo/src/test/java/browsers/Browsers.java
@@ -1,21 +1,28 @@
 package browsers;
 
+import java.time.Duration;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.devtools.v85.browser.Browser;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.edge.EdgeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
 
 public class Browsers {
 
 	protected static WebDriver driver;
 
+	@BeforeClass
 	public static void abrirNavegador() {
 
 		String site = "http://sampleapp.tricentis.com";
-		String navegador = "edge";
+		String navegador = "Edge";
 
 		if (navegador.equalsIgnoreCase("chrome")) {
 			System.setProperty("webdriver.chrome.driver", "./Drivers/chromedriver.exe");
@@ -34,11 +41,11 @@ public class Browsers {
 			WebDriver firefoxDriver = new FirefoxDriver(firefoxOptions);
 			driver = firefoxDriver;
 			driver.manage().timeouts().implicitlyWait(java.time.Duration.ofSeconds(10));
-		} else if (navegador.equalsIgnoreCase("edge")) {
+		} else {
 			System.setProperty("webdriver.edge.driver", "./Drivers/msedgedriver.exe");
 			EdgeOptions edgeOptions = new EdgeOptions();
 			edgeOptions.setCapability("useAutomationExtension", false);
-			edgeOptions.setHeadless(false);
+			edgeOptions.setHeadless(true);
 			edgeOptions.addArguments("--disable-gpu");
 			edgeOptions.addArguments("--window-size=1400,800");
 			WebDriver edgeDriver = new EdgeDriver(edgeOptions);
@@ -48,6 +55,7 @@ public class Browsers {
 		driver.manage().window().maximize();
 	}
 
+	@AfterClass
 	public static void fecharNavegador() {
 
 		driver.quit();
