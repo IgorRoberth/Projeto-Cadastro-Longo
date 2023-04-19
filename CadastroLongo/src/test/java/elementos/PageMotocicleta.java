f576c9c500f6a95c66095485a8d7e3fa32da9615
Igor Roberth <96749911+IgorRoberth@users.noreply.github.com>
2023-04-19 00:27:43 -0300

    Atualizar o PageMotocicleta.java

--- CadastroLongo/src/test/java/elementos/PageMotocicleta.java
+++ CadastroLongo/src/test/java/elementos/PageMotocicleta.java
@@ -4,10 +4,10 @@ import org.openqa.selenium.By;
 
 public class PageMotocicleta {
 	
-	        public static By modeloMoto = By.cssSelector("#model > option:nth-child(5)");
-		public static By cilindroMoto = By.cssSelector("#cylindercapacity");
-		public static By assentMoto = By.cssSelector("#numberofseatsmotorcycle > option:nth-child(3)");
-		public static By ocupaMoto = By.cssSelector("#occupation > option:nth-child(6)");
-		public static By HobbMoto = By.cssSelector("#insurance-form > div > section:nth-child(2) > div.field.idealforms-field.idealforms-field-checkbox > p > label:nth-child(1) > span");		
-		public static By somaSegMoto = By.xpath("//*[text()=' 3.000.000,00']");
+	   public static By modeloMoto = By.cssSelector("#model > option:nth-child(5)");
+	public static By cilindroMoto = By.cssSelector("#cylindercapacity");
+	public static By assentMoto = By.cssSelector("#numberofseatsmotorcycle > option:nth-child(3)");
+	public static By ocupaMoto = By.cssSelector("#occupation > option:nth-child(6)");
+	public static By HobbMoto = By.cssSelector("#insurance-form > div > section:nth-child(2) > div.field.idealforms-field.idealforms-field-checkbox > p > label:nth-child(1) > span");		
+	public static By somaSegMoto = By.xpath("//*[text()=' 3.000.000,00']");
 }
