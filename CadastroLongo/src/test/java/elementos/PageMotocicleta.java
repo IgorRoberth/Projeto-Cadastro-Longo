010a42eb9476b53f069e8c8355e9815163941d22
Igor Roberth <igorroberthm@gmail.com>
2023-04-19 18:41:59 -0300

    Concluido atualização da nova branch

--- CadastroLongo/src/test/java/elementos/PageMotocicleta.java
+++ CadastroLongo/src/test/java/elementos/PageMotocicleta.java
@@ -4,17 +4,9 @@ import org.openqa.selenium.By;
 
 public class PageMotocicleta {
 	
-<<<<<<< HEAD
 		public static By cilindroMoto = By.cssSelector("#cylindercapacity");
 		public static By ocupaMoto = By.cssSelector("#occupation > option:nth-child(6)");
 		public static By HobbMoto = By.cssSelector("#insurance-form > div > section:nth-child(2) > div.field.idealforms-field.idealforms-field-checkbox > p > label:nth-child(1) > span");		
 		public static By somaSegMoto = By.xpath("//*[text()=' 3.000.000,00']");
-=======
-	public static By modeloMoto = By.cssSelector("#model > option:nth-child(5)");
-	public static By cilindroMoto = By.cssSelector("#cylindercapacity");
-	public static By assentMoto = By.cssSelector("#numberofseatsmotorcycle > option:nth-child(3)");
-	public static By ocupaMoto = By.cssSelector("#occupation > option:nth-child(6)");
-	public static By HobbMoto = By.cssSelector("#insurance-form > div > section:nth-child(2) > div.field.idealforms-field.idealforms-field-checkbox > p > label:nth-child(1) > span");		
-	public static By somaSegMoto = By.xpath("//*[text()=' 3.000.000,00']");
->>>>>>> 2ba5ccc31e6d446a1407cc9c7c6f0eeb87fa9d7f
+
 }
