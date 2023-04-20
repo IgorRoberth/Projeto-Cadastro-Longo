83cbf343f0ab34d52be2734ee17bf8b5f16e8c1f
Igor Roberth <96749911+IgorRoberth@users.noreply.github.com>
2023-04-20 16:41:06 -0300

    Update PageMotocicleta.java

--- CadastroLongo/src/test/java/elementos/PageMotocicleta.java
+++ CadastroLongo/src/test/java/elementos/PageMotocicleta.java
@@ -4,8 +4,8 @@ import org.openqa.selenium.By;
 
 public class PageMotocicleta {
 
-    public static By cilindroMoto = By.cssSelector("#cylindercapacity");
-		public static By ocupaMoto = By.cssSelector("option[value=\"Selfemployed\"]");
-		public static By HobbMoto = By.xpath("//*[text()=' Cliff Diving']");
-		public static By somaSegMoto = By.xpath("//*[text()=' 3.000.000,00']");
+       public static By cilindroMoto = By.cssSelector("#cylindercapacity");
+       public static By ocupaMoto = By.cssSelector("option[value=\"Selfemployed\"]");
+       public static By HobbMoto = By.xpath("//*[text()=' Cliff Diving']");
+       public static By somaSegMoto = By.xpath("//*[text()=' 3.000.000,00']");
 }
